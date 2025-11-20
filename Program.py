48e848d44d66454079ab75d95263237c6d44ac77
Bryn-Elliott <bryn.elliott99@gmail.com>
2025-11-20 16:43:36 +0000

    error correction in naiming conventions to allow xlsx reading v0.15

--- Program.py
+++ Program.py
@@ -3,8 +3,8 @@ import pandas as pd
 # import numpy as np
 # import matplotlib.pyplot as plt
 
-file_path_ADF = "Input Data\ADF Input Data.xlsx"
-file_path_Pop = "Input Data\Populus Data Input.xlsx"
+file_path_ADF = "InputData\AdfInputData.xlsx"
+file_path_Pop = "InputData\PopulusInputData.xlsx"
 
 df_ADF = pd.read_excel(file_path_ADF, sheet_name="Table 2")
 df_ADF_str = df_ADF.astype(str)
