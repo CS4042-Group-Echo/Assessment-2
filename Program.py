13fb8afd89805e82ffddc60866e535841f3b0cc7
Bryn-Elliott <bryn.elliott99@gmail.com>
2025-11-20 14:42:01 +0000

    Editing .py for xslx reading v 0.14

--- Program.py
+++ Program.py
@@ -6,8 +6,8 @@ import pandas as pd
 file_path_ADF = "Input Data\ADF Input Data.xlsx"
 file_path_Pop = "Input Data\Populus Data Input.xlsx"
 
-df_ADF = pd.read_excel(file_path_ADF, sheet_name="Data")
+df_ADF = pd.read_excel(file_path_ADF, sheet_name="Table 2")
 df_ADF_str = df_ADF.astype(str)
 
-df_Pop = pd.read_excel(file_path_Pop, sheet_name="Data")
+df_Pop = pd.read_excel(file_path_Pop, sheet_name="Table 1")
 df_Pop_str = df_Pop.astype(str)
