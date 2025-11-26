a0ea8900b64e4efa8e8a8d7e7aadcb4ce1528f5c
Bryn-Elliott <bryn.elliott99@gmail.com>
2025-11-26 12:01:14 +0000

    Adding data column removal for unecessary data

--- Program.py
+++ Program.py
@@ -1,17 +1,31 @@
 import pandas as pd
-# import openpyxl
+import openpyxl
 # import numpy as np
 # import matplotlib.pyplot as plt
 
 file_path_ADF = "InputData\AdfInputData.xlsx"
 file_path_Pop = "InputData\PopulusInputData.xlsx"
+file_path_Clean_ADF = "CleanData\AdfCleanData.xlsx"
+file_path_Clean_Pop = "CleanData\PopulusCleanData.xlsx"
+file_path_Output_ADF = "OutputData\AdfOutputData.xlsx"
+file_path_Output_Pop = "OutputData\PopulusOutputData.xlsx"
 
-df_ADF = pd.read_excel(file_path_ADF, sheet_name="Table 2")
-df_ADF_str = df_ADF.astype(str)
+i = 0
+wb = openpyxl.load_workbook(file_path_Clean_ADF)
+for sheet in wb.sheetnames:
+    ws = wb[sheet]
+    if i > 0:
+        ws.delete_cols(5, 10)
+    i += 1
+wb.save(file_path_Output_ADF)
 
-df_Pop = pd.read_excel(file_path_Pop, sheet_name="Table 1")
-df_Pop_str = df_Pop.astype(str)
+i = 0
+wb = openpyxl.load_workbook(file_path_Clean_Pop)
+for sheet in wb.sheetnames:
+    ws = wb[sheet]
+    if i > 0:
+        ws.delete_cols(20, 1)
+        ws.delete_cols(2, 4)
+    i += 1
+wb.save(file_path_Output_Pop)
 
-
-print("ADF DataFrame (as strings):")
-print(df_Pop_str)
